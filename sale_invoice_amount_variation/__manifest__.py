5296c34951823aa2b096abe224faa5f3078c9d48
Odoo Aktiv <solankiriddhir@yahoo.in>
2018-09-06 11:25:47 +0530

    [ADD] Initial commit for SaleInvoiceAmountVariation for branch 10.0

---
+++ sale_invoice_amount_variation/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of AktivSoftware See LICENSE file for full copyright and licensing details.
+{
+    'name': "Sale Invoice Amount Variation",
+    'summary': """
+        Prints report which shows unit price changed, sale price, difference and the user who has done changes.
+       """,
+    'author': "Aktiv Software",
+    'website': "http://www.aktivsoftware.com",
+    'license': "AGPL-3",
+    'category': 'web',
+    'version': '10.0.1.0.0',
+    'depends': ['sale', 'account'],
+    'data': [
+        'views/account_invoice_line_views.xml',
+        'views/account_invoice.xml',
+        'views/report_invoice.xml'
+    ],
+    'images': [
+        'static/description/banner.jpg',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False
+}
